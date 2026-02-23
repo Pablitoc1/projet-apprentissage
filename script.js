73a361318904b36304e3499713677c5f8cd19661
Pablitoc1 <5fhrxf5yzn@privaterelay.appleid.com>
2026-02-23 10:40:06 +0100

    #Update 2

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let bouton = document.getElementById("monBouton");
 bouton.addEventListener("click", function() {
     document.body.classList.toggle("mode-sombre");
 });
+
+// On sélectionne toutes les cartes
+let toutesLesCartes = document.querySelectorAll('.carte');
+
+// Pour chaque carte, on écoute le clic
+toutesLesCartes.forEach(carte => {
+    carte.addEventListener('click', function() {
+        // On cherche le paragraphe 'details' à l'intérieur de CETTE carte
+        let detail = this.querySelector('.details');
+        // On alterne l'affichage
+        detail.classList.toggle('affiche-details');
+    });
+});
