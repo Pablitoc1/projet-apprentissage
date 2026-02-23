ad093511e2d4c3eaf2b83ccf2d7736f89c143798
Pablitoc1 <5fhrxf5yzn@privaterelay.appleid.com>
2026-02-23 23:21:57 +0100

    Update 11

--- script.js
+++ script.js
@@ -107,6 +107,19 @@ const cartePokemon = document.getElementById("carte-pokemon");
 const nomPokemon = document.getElementById("nom-pokemon");
 const imagePokemon = document.getElementById("image-pokemon");
 const typesPokemon = document.getElementById("types-pokemon");
+const statsPokemon = document.getElementById("stats-pokemon");
+const couleursTypes = {
+    fire: "#FF421C",
+    water: "#2980ef",
+    grass: "#3fa129",
+    electric: "#fac000",
+    psychic: "#ef4179",
+    ice: "#74cfaf",
+    dragon: "#5060e1",
+    dark: "#624d4e",
+    fairy: "#ef70ef",
+    normal: "#919aa2"
+};
 
 
 function obtenirPokemon(nom) {
@@ -121,6 +134,25 @@ function obtenirPokemon(nom) {
             typeElement.classList.add("type");
             typeElement.textContent = type.type.name;
             typesPokemon.appendChild(typeElement);
+            const typePrincipalPokemon = donnees.types[0].type.name
+            if (couleursTypes[typePrincipalPokemon]) {
+                const couleur = couleursTypes[typePrincipalPokemon];
+                cartePokemon.style.backgroundColor = couleur;
+            }
+
+        });
+        statsPokemon.innerHTML ="";
+        donnees.stats.forEach(stat => {
+            const ligneStats = document.createElement("div");
+            const statElement = document.createElement("span");
+            const barreStats = document.createElement("progress");
+            barreStats.value = stat.base_stat;
+            barreStats.max = 255;
+            statElement.classList.add("stat");
+            statElement.textContent = stat.stat.name;
+            ligneStats.appendChild(statElement);
+            ligneStats.appendChild(barreStats);
+            statsPokemon.appendChild(ligneStats);
         });
       })
       .catch(erreur => {
